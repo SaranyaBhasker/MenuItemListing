85aac7eda22c12033a1a338dc559b4a33103b778
Saranya B <saranyaharishbm@gmail.com>
2022-06-18 00:10:08 +0530

    Add project files.

---
+++ Controllers/MenuItemController.cs
@@ -0,0 +1,39 @@
+﻿using MenuItemListing.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuItemListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        [HttpGet]
+        public List<MenuItem> GetItem()
+        {
+            List<MenuItem> MenuList = new List<MenuItem>()
+            {   new MenuItem() {Id=1, Name="Ferrari", Active=false, DateOfLaunch=new DateTime(2022,06,09), freeDelivery=false,Price=500000},
+                new MenuItem() {Id=2, Name="BMW", Active=true, DateOfLaunch=new DateTime(2022,10,03), freeDelivery=false,Price=600000}
+                
+            };
+            return MenuList;
+        }
+
+        [HttpGet("{id}")]
+        public MenuItem GetItemById(int id)
+        {
+            List<MenuItem> MenuList = new List<MenuItem>()
+            {   new MenuItem() {Id=1, Name="Ferrari", Active=false, DateOfLaunch=new DateTime(2022,06,09), freeDelivery=false,Price=500000},
+                new MenuItem() {Id=2, Name="BMW", Active=true, DateOfLaunch=new DateTime(2022,10,03), freeDelivery=false,Price=600000}
+
+            };
+            MenuItem obj = MenuList.SingleOrDefault(item => item.Id == id);
+            return obj;
+
+        }
+    }
+}
